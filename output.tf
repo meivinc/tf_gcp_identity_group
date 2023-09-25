fe63b8d987ae484d8f2db582edb8597675c23a99
meivinc <70801412+meivinc@users.noreply.github.com>
2023-09-25 18:02:55 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,17 +1,3 @@
-/*
-
-Output Of the terraform file
-
-*/
-
-
-# output "Created_groups"{
-#     description = "group creation"
-#     # value       = { for vm in (var.group_id) : vm => module.group[vm].display_name }
-#     value = var.group_id
-
-# }
-
 output "Created_groups"{
     description = "group creation"
     value       =  module.group[*]
